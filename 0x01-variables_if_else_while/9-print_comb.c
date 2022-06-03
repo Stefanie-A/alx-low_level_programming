c6319e1234ed9c8fcefbcee3613684517f111768
Stephanie Albert <albertstephanie630@gmail.com>
2022-06-03 12:11:49 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 /**
- * main - Print single digit numbers
+ * main - Entry point
  *
- * Description: Separated by comma
- * Return: Always(0) Success
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
+
 {
-	int lower;
-	int upper;
+int number;
 
-	upper = 9;
 
-	for (lower = 0; lower <= upper; lower++)
-	{
-		putchar(lower + '0');
-		putchar(',');
-		putchar(' ');
-	}
-	putchar('\n');
-	return (0);
+for (number = 48; number <= 57; number++)
+{
+putchar(number);
+
+if (number == 57)
+{
+break;
+}
+
+putchar(',');
+putchar (' ');
+}
+
+putchar('\n');
+
+return (0);
+
 }
