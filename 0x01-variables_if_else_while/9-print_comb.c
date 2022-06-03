524a13ade49a8aafde19976c54c8783a6e1c7dfc
Stephanie Albert <albertstephanie630@gmail.com>
2022-06-03 12:02:24 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+ble File  23 lines (21 sloc)  321 Bytes
+#include <stdio.h>
+/**
+ * main - Print single digit numbers
+ *
+ * Description: Separated by comma
+ * Return: Always(0) Success
+ */
+int main(void)
+{
+	int lower;
+	int upper;
+
+	upper = 9;
+
+	for (lower = 0; lower <= upper; lower++)
+	{
+		putchar(lower + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
