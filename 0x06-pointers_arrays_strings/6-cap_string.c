d5557f83bf385f492e0952094c7b7a39c4dd302c
Stefanie-A <albertstephanie630@gmail.com>
2022-06-16 02:08:10 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,43 @@
 #include "main.h"
+
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase
- * @str: string
- * Return: returns char
- */
+ * *cap_string - capitalize words
+ * @str: pointer
+ * Return: capitalzied string
+*/
 
-
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
-int i;
+char sep[] = ",\t;\n; .!?\"(){}";
+int flag, i, ii;
 
 for (i = 0; str[i] != '\0'; i++)
 {
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - 32;
-}
-}
+	flag = 0;
 
+	if (i == 0)
+	{
+		flag = 1;
+	}
+	else
+	{
+		for (ii = 0; sep[ii] != '\0'; ii++)
+		{
+			if (str[i - 1] == sep[ii])
+			{
+				flag = 1;
+				break;
+			}
+		}
+	}
+
+	if (flag == 1)
+	{
+		if (str[i] <= 'z' && str[i] >= 'a')
+		{
+			str[i] -= ('a' - 'A');
+		}
+	}
+}
 return (str);
 }
